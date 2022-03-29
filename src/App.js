867f5ac8bacb01a685119c4149558e35e4549e7b
Marco <mar.manunta@gmail.com>
2022-03-29 17:32:41 +0200

    "committed my ass"

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import SingleAnimal from './components/SingleAnimal';
 
-const App = () => {
-  console.log(process.env);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/single/:id/:searchTerm' element={<SingleAnimal />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/single/:id/:searchTerm' element={<SingleAnimal />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
